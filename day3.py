d2fc564d56fad964bfeb912917a91bf517fca39b
shubham570 <51357796+shubham570@users.noreply.github.com>
2019-06-08 14:02:53 +0530

    Add files via upload

---
+++ day3.py
@@ -0,0 +1,13 @@
+#!usr/bin/python3
+
+# now importing time library
+import time
+local ='''
+Press 1 to start your vlc media player :
+Press 2 to play your fav song in youtube :
+Press 3 to search something on google :
+'''
+print(local)
+	
+choice=input()
+print(choice)
